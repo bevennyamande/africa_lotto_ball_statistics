05c559f76a8cad67a05c3984ce159b1e17299e81
bevennyamande <bevennyamande@gmail.com>
2019-04-04 22:44:47 +0200

    commit

---
+++ main.py
@@ -0,0 +1,33 @@
+import requests
+import pygal
+from bs4 import BeautifulSoup
+
+# two most interesting urls
+africa_lotto__url = 'https://africalotto.co.zw/statistics-bonus-lotto-two'
+
+def scrap_africa_lotto(africa_lotto__url: str):
+    bonusdata = []
+    try:
+        balls = requests.get(africa_lotto__url)
+        soup = BeautifulSoup(balls.text, 'html.parser')
+        bonus_ball_table = bonus_soup.find('table').get_text()
+        bonusdata = [line for line in bonus_ball_table.split('\n') if line.strip()]
+        with open('bonusdata.txt', 'a+') as fileobj:
+            fileobj.write(bonus_ball_table)
+    except Exception as e:
+        print(e)
+    return bonusdata
+
+
+def clean_saved_data(filename):
+    with open(filename, 'w+') as fileobj:
+        for line in fileobj.readline():
+            print(line)
+
+if __name__ == '__main__':
+    # scrap_africa_lotto(africa_lotto__url)
+    clean_saved_data('bonusdata.txt')
+
+# TODO: create an excel sheet or plot the results in a graph
+# preferable pygal and renter it to a website
+
